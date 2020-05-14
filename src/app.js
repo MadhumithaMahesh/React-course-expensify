52bfb493d62ef242eeb46b0b8aaa28331a8d73ff
MadhumithaMahesh <bmmadhumitha@gmail.com>
2020-05-14 12:46:26 +0530

    Removed Dummy

--- src/app.js
+++ src/app.js
@@ -13,13 +13,13 @@ import getVisibleExpenses from '../src/selectors/expenses'
 
 const store = configureStore()
 
-store.dispatch(addExpense({description:'rent',amount:500000}))
-store.dispatch(addExpense({description:'water bill',amount:500}))
-store.dispatch(addExpense({description:'gas bill',amount:1000}))
-console.log(store.getState())
-const state = store.getState()
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
-console.log(visibleExpenses)
+// store.dispatch(addExpense({description:'rent',amount:500000}))
+// store.dispatch(addExpense({description:'water bill',amount:500}))
+// store.dispatch(addExpense({description:'gas bill',amount:1000}))
+// console.log(store.getState())
+// const state = store.getState()
+// const visibleExpenses = getVisibleExpenses(state.expenses,state.filters)
+// console.log(visibleExpenses)
 
 const jsx = (
 <Provider store ={store}>
